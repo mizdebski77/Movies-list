6f7ab2168e896af869aaff0570c9e9375f6309be
Marcin Izdebski <mizdebski123@gmail.com>
2024-10-07 16:51:20 +0200

    normalize css added

---
+++ src/core/globalStyles.ts
@@ -0,0 +1,20 @@
+
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+html {
+    box-sizing: border-box;
+  }
+  
+  *,
+  ::after,
+  ::before {
+    box-sizing: inherit;
+  }
+  
+body {
+  word-break: break-word;
+  background: ${({ theme }) => theme.color.mainColor};
+  font-family: 'Playfair Display', serif;
+  margin: 0;
+}`;
---
+++ src/core/theme.ts
@@ -0,0 +1,16 @@
+export const theme = {
+    color: {
+        mainColor: "#EFEFEF;",
+       // secondColor: "rgb(49, 103, 48)",
+        secondColor: "rgb(29, 105, 24)",
+        hoverSecondColor: 'rgba(15,119,1, 0.9)',
+        fontColor: "#000000",
+        thirdColor: '#868686',
+    },
+
+    breakPoint: {
+        firstBreakPoint: 1380,
+        secondBreakPoint: 1200,
+        mobileMax: 767,
+    }
+};
